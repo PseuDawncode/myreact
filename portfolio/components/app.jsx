134e49d10e15afea1c28564825ad6fefc9639af7
Mary Dawn Malida <157961578+PseuDawncode@users.noreply.github.com>
2025-05-13 18:29:17 +0200

    added component for projectData

--- portfolio/components/app.jsx
+++ portfolio/components/app.jsx
@@ -2,45 +2,7 @@ import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import ProjectCard from './projectCard';
 import NavigationBar from './navigationBar';
-
-const projectData = [
-  {
-    name: "Construction Services App",
-    screenshot: "/images/tranquility.png",
-    github: "https://github.com/PseuDawncode/My-Project",
-    techUsed: "HTML, CSS, and JavaScript",
-    writeup: {
-      what: "A web app that shows available services for the clients and simply can book an appointment.",
-      learned: "My very first time to use JavaScript for the services—dropdown button.",
-      role: "Solo project",
-      challenges: "Handling form submission validation.",
-    }
-  },
-  {
-    name: "Book List App",
-    screenshot: "/images/BookListApp.png",
-    github: "https://github.com/PseuDawncode/LibraryArray",
-    techUsed: "HTML, JavaScript, and Bootstrap",
-    writeup: {
-      what: "A simple book list app to add and delete books. Simply can also mark the book as Read or Unread",
-      learned: "How to store data in a Local Storage",
-      role: "Solo project",
-      challenges: "To filter the Read and Unread books to show.",
-    }
-  },
-  {
-    name: "Tattoo Website",
-    screenshot: "/images/tattooPage.png",
-    github: "https://github.com/lZ3R0l/tattoo-project",
-    techUsed: "HTML and CSS",
-    writeup: {
-      what: "A web app for tattoo client that simply shows tattoo sample—images and where they can also book an appointment.",
-      learned: "Teamwork, Communication, and Collaboration",
-      role: "Group project",
-      challenges: "Making it mobile-responsive and accessible.",
-    }
-  }
-];
+import projectData from './projects';
 
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
